058f799d4fe16b6a6c9bd52d63f2088e19d1784f
Chisom Jude <judegodstimechisom@gmail.com>
2025-04-09 00:29:15 +0100

    updated

---
+++ app/database.py
@@ -0,0 +1,10 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client.cloud_native
+students_collection = db.students
