39c9dbc52541d181711bc68166450dc3d8c72867
Chisom Jude <judegodstimechisom@gmail.com>
2025-05-17 08:28:07 +0100

    Updated database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import motor.motor_asyncio
 import hvac
 
 # Connect to Vault
-vault_client = hvac.Client(url='http://102.37.142.227:8200')
+vault_client = hvac.Client(url='http://<vaultIP>:8200') #Ip will will be provided in the class or use localhost if you have a vault server on you host maching
 vault_client.token = os.getenv("VAULT_TOKEN")
 
 read_response = vault_client.secrets.kv.v2.read_secret_version(path="student01")
